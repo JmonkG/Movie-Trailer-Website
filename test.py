8a14173422f3d6721d15f2a6633dd1a48715e77d
Carlos Jordan <jjordan84@gmail.com>
2015-03-20 12:42:33 -0500

    Information of Movies
    Added real information of movies and 1 try to create the page

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 from movie import Movie
-"""import fresh_tomatoes"""
+import fresh_tomatoes
 
-m = Movie('Prueba','http:','http2:',20,'Comedy','fun',1994,'me')
-m.add_actors(['melina','mariloli','josue','diana','walter'])
-m.add_movie_tags(['Quevedo','Ciudad del Rio','Gye'])
+m = []
+m.append(Movie("Shidler's List",'http://goo.gl/D9A7ak','https://www.youtube.com/watch?v=JdRGC-w9syA',20,'Comedy','fun',1994,'me'))
+m.append(Movie('The Matrix','http://goo.gl/oZNWwv','https://www.youtube.com/watch?v=JdRGC-w9syA,20','Comedy','fun',1994,'me'))
+m.append(Movie('Avengers','http://goo.gl/DuPSZI','http://goo.gl/r9lAzm',20,'Comedy','fun',1994,'me'))
+m.append(Movie('UP','http://goo.gl/zcZHiL','http://goo.gl/Vq8IBQ',20,'Comedy','fun',1994,'me'))
+m.append(Movie('American Sniper','http://goo.gl/i9CBY8','http://goo.gl/WSBLdc',20,'Comedy','fun',1994,'me'))
 
-print m.title,m.actors,m.tags
+open_movies_page(m)
