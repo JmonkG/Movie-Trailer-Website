78c41f8c39d45857b297e12db7b2a24b1826b5ab
Carlos Jordan <jjordan84@gmail.com>
2015-03-20 16:39:51 -0500

    Website Working 1.1
    Added new movies, changes in css and style
    configuring it to retrieve from file

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -6,7 +6,7 @@ import re
 main_page_head = '''
 <head>
     <meta charset="utf-8">
-    <title>Fresh Tomatoes!</title>
+    <title>My Favorite Movies</title>
 
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
@@ -37,7 +37,7 @@ main_page_head = '''
             padding-top: 20px;
         }
         .movie-tile:hover {
-            background-color: #EEE;
+            background-color: #99CCFF;          /* Change the color of the background for each movie*/
             cursor: pointer;
         }
         .scale-media {
@@ -105,7 +105,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">My Favorite Movie Trailers</a>
           </div>
         </div>
       </div>
@@ -122,6 +122,8 @@ movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
+    <!-- this are added fields from the data structure -->
+    <h3>{movie_genre}</h3>
 </div>
 '''
 
@@ -138,7 +140,8 @@ def create_movie_tiles_content(movies):
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
+            movie_genre = movie.genre
         )
     return content
 
---
+++ test2.py
@@ -0,0 +1,5 @@
+from movie import Movie
+#from fresh_tomatoes import *
+
+m = open("")
+#open_movies_page(m)
