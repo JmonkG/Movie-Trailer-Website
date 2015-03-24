a71cb152f5d734e7e3855115af98d86849ca43c4
Carlos Jordan <jjordan84@gmail.com>
2015-03-24 07:04:45 -0500

    Minor Fixes
    Follow the recommendations for the project.

--- movie.py
+++ movie.py
@@ -1,10 +1,10 @@
-''' The class Movie represents and object called Movie.
+class Movie(object):
+    ''' The class Movie represents and object called Movie.
     As it is expected, it has fields such as title, poster and trailer URL.
     In order to go a little further, I add the fields runtime, the duration of the movie
     its genre, the year it was presented, the director and for further information, a link to the imdb website
     for the movie
-'''
-class Movie(object):
+    '''
     # Defined a constructor for a Movie Object, where the attributes of the object are taken from a List called movie_params_list.
     def __init__(self, movie_params_list):
         self.title = movie_params_list[0]
